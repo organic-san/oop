49f2cf7623a48b3c1c1b0fa1cf9f73c3606f63f5
s11145 s11145 <s11145@oop.cs.nccu.edu.tw>
2023-09-19 03:10:38 +0800

    init

---
+++ assign0/STest.cc
@@ -0,0 +1,17 @@
+/*
+ * File: STest.cc * Purpose: Sample testing program for the hello program
+ * Author: Tsai-Yen Li (li@cs.nccu.edu.tw) 8/28/96
+ *         (You need put your ID here as well.)
+ */
+
+#include <iostream>
+#include "Hello.h"
+
+int
+main() {
+  // Write your code below...
+  PrintHello();
+  std::cout << "ID: 111703045\n"; // Please replace your student ID.
+  return 0;
+
+}
