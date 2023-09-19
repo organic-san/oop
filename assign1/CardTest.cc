214dd25c23ebd6a49dd95bd715368361db3b616f
organic_san <yuki.0710.happy@gmail.com>
2023-09-19 08:33:53 +0800

    assign1

---
+++ assign1/CardTest.cc
@@ -0,0 +1,87 @@
+/* Copyright (c) 1999 by The National Chengchi Univ. All Rights Reserved */
+
+/***************************************************************************
+   NAME
+     CardTest.cc
+   PURPOSE
+     Testing card displaying
+   NOTES
+
+   AUTHOR
+     Tsai-Yen Li (li@cs.nccu.edu.tw)
+   HISTORY
+     Tsai-Yen Li - Oct 7, 1999: Created.
+***************************************************************************/
+
+/* $Log:$ */
+
+extern "C" {
+#include <stdio.h>
+#include <stdlib.h>
+#include <strings.h>
+#include <assert.h>
+}
+
+#include <iostream>
+#include "AnsiPrint.h"
+#include "Cards.h"
+
+using namespace std;
+
+/**
+ * Print my id 
+ */
+void
+PrintMyID(const char *studId) {
+
+  char *str= new char[sizeof(studId)+5];
+  // we put string in a buffer first 
+  sprintf(str,"ID: %s",studId);
+  // print the buffer out with color
+  AnsiPrint(str,yellow,red, true, true);
+  cout << endl << endl;
+  delete [] str;
+
+}
+
+
+/**
+ * PrintUsage
+ */
+void
+PrintUsage(const char *progName) {
+
+  assert(progName);
+  cout << "Usage: " << progName << " NCards [Seed]" << endl;
+
+}
+
+/**
+ * Main Function Call
+ */
+int
+main(int argc, char **argv) {
+
+  int ncards=0;  // number of cards to draw 
+  if (argc==3) {
+    long seed=atoi(argv[2]); // the third argument is the seed
+    srand(seed);
+    ncards=atoi(argv[1]); // the second argument is the number of cards
+  } else if (argc==2) {
+    ncards=atoi(argv[1]);
+  } else {
+    PrintUsage(argv[0]);
+    exit(-1);
+  }
+
+  // You are asked to fill in a few missing statements here
+
+  
+
+
+  // Print my id at the end of the program
+  PrintMyID("u1234567");
+
+  return 0;
+
+}
---
+++ assign1/Cards.cc
@@ -0,0 +1,11 @@
+/* Copyright (c) 1999 by The National Chengchi Univ. All Rights Reserved */
+
+/***************************************************************************
+   NAME
+     Cards.cc
+   NOTE
+     You are asked to design this module.
+     You need to draw the given number of cards and print them out.
+     Assume that you are given a deck of 52 cards. The cards do not reappear.
+     That is, you do not put a card back into the deck after it is drawn.
+***************************************************************************/
